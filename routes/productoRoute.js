84922b9a53157aa5a396d840e99aab10342252ff
Yeisonata <tobon055@gmail.com>
2024-01-20 22:09:32 -0500

    En este commit, se han definido rutas para actualizar, eliminar y crear productos en el archivo de rutas. Cada ruta utiliza el middleware 'authMiddleware' para verificar la autenticación y 'esAdmin' para validar los permisos de administrador antes de ejecutar la lógica de actualización, eliminación o creación de productos.

--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -2,17 +2,29 @@
 const express = require("express");
 
 // Importar el controlador de productos
-const { crearProducto, obtenerProducto, getAllProductos, actualizarProducto, eliminarProducto } = require("../controller/productoController");
+const {
+  crearProducto,
+  obtenerProducto,
+  getAllProductos,
+  actualizarProducto,
+  eliminarProducto,
+} = require("../controller/productoController");
+
+// Importa las funciones 'esAdmin' y 'authMiddleware' desde el módulo 'authMiddleware'
+// authMiddleware: Verifica autenticación y detiene si falla; permite al siguiente middleware si es exitosa.
+// esAdmin: Verifica permisos de administrador y detiene si no es un administrador; permite al siguiente middleware si es un administrador.
+const { esAdmin, authMiddleware } = require("../middleware/authMiddleware");
 
 // Crear una instancia de Router de express
 const router = express.Router();
 
 // Ruta POST para la creación de productos, utiliza el controlador crearProducto
-router.post("/", crearProducto);
+router.post("/", authMiddleware, esAdmin, crearProducto);
 // Ruta GET para obtener un producto por id, utilizando el controlador  obtenerProducto
 router.get("/:id", obtenerProducto);
-router.put("/:id", actualizarProducto);
-router.delete("/:id", eliminarProducto);
+
+router.put("/:id", authMiddleware, esAdmin, actualizarProducto);
+router.delete("/:id", authMiddleware, esAdmin, eliminarProducto);
 router.get("/", getAllProductos);
 
 // Exportar el router para su uso en la aplicación principal
