b8bf5856d666ec9154cbea8cae295acf8e93431c
Yeisonata <tobon055@gmail.com>
2024-01-28 17:52:34 -0500

    Hemos actualizado UsuarioModelo:
    - Se ha añadido la funcionalidad para manejar la renovación de contraseña.
    - Se ha introducido un campo 'cambiarContrasenia' para rastrear la fecha de cambio de contraseña.
    - Se ha agregado 'actualizarContraseniaToken' para almacenar el token de renovación de contraseña.
    - Se ha incluido 'expiracionRenovacionContrasenia' para gestionar la caducidad del token de renovación de contraseña.
    - Se ha ajustado el pre-save para verificar si la contraseña ha sido modificada antes de aplicar el hash.

--- models/usuarioModelo.js
+++ models/usuarioModelo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema(
   {
@@ -29,11 +30,10 @@ var userSchema = new mongoose.Schema(
     roles: {
       type: String,
       default: "usuario",
-
     },
     estaBloqueado: {
       type: Boolean,
-      default:false,
+      default: false,
     },
     carrito: {
       type: Array,
@@ -41,10 +41,12 @@ var userSchema = new mongoose.Schema(
     },
     direccion: [{ type: mongoose.Schema.Types.ObjectId, ref: "Direccion" }],
     listaDeDeseos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Producto" }],
-    refrescarToken:{
-      type:String,
-      
-    }
+    refrescarToken: {
+      type: String,
+    },
+    cambiarContrasenia: Date,
+    actualizarContraseniaToken: String,
+    expiracionRenovacionContrasenia: Date,
   },
   {
     timestamps: true,
@@ -53,13 +55,27 @@ var userSchema = new mongoose.Schema(
 
 //Para encriptar la contraseña
 userSchema.pre("save", async function (next) {
+  //Para comprobar si la contraseña fue modificada
+  if (!this.esModificada("contrasenia")) {
+    next();
+  }
   const salt = await bcrypt.genSaltSync(10);
   this.contrasenia = await bcrypt.hash(this.contrasenia, salt);
+  next();
 });
 
 //Método para que la contraseña encriptada coincida con la ingresada
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.contrasenia);
+};
+userSchema.methods.createPasswordRestToken = async function () {
+  const cambiartoken = crypto.randomBytes(32).toString("hex");
+  this.actualizarContraseniaToken = crypto
+    .createHash("sha256")
+    .update(cambiartoken)
+    .digest("hex");
+  this.expiracionRenovacionContrasenia = Date.now() + 30 * 60 * 1000; //10 minutos
+  return cambiartoken;
 };
 
 //Export the model
