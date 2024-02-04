299c8a312cca2ca40e99a626d5b7b979e5a73ccc
Yeisonata <tobon055@gmail.com>
2024-02-03 22:21:29 -0500

    Se añadieron funciones adicionales en el controlador blogController y se configuraron las rutas correspondientes en blogRoutes:
    Controlador blogController:
    
    Se implementó la función 'obtenerBlog' para obtener detalles específicos de un blog mediante su identificador único.
    Se creó 'obtenerAllBlogs' para obtener la lista de todos los blogs disponibles.
    Se añadió 'eliminarBlog' para eliminar un blog según su identificador único.
    Estas funciones permiten obtener detalles de un blog, obtener todos los blogs y eliminar blogs, respectivamente.
    Rutas blogRoutes:
    
    Se agregaron nuevas rutas para las funciones implementadas en el controlador, proporcionando así la capacidad de obtener detalles de un blog, obtener todos los blogs y eliminar blogs.

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,18 +1,17 @@
-const Blog = require("../models/blogModelo")
-const Usuario = require("../models/usuarioModelo")
-const asyncHandler = require("express-async-handler")
+const Blog = require("../models/blogModelo");
+const Usuario = require("../models/usuarioModelo");
+const asyncHandler = require("express-async-handler");
 const validarMongoDbId = require("../utils/validarMongodbId");
 
-const crearBlog = asyncHandler(async(req,res)=>{
-try {
-  const nuevoBlog = await Blog.create(req.body)
-  res.json(
-    nuevoBlog,
-  )
-} catch (error) {
-  throw new Error(error)
-}
-})
+const crearBlog = asyncHandler(async (req, res) => {
+  try {
+    const nuevoBlog = await Blog.create(req.body);
+    res.json(nuevoBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 const actualizarBlog = asyncHandler(async (req, res) => {
   const { id } = req.params;
   try {
@@ -24,5 +23,39 @@ const actualizarBlog = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
+const obtenerBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const obtenerBlog = await Blog.findById(id);
+    const actualizarVisitas = await Blog.findByIdAndUpdate(
+      id,
+      {
+        $inc: { numeroVisitas: 1 },
+      },
+      { new: true }
+    );
+    res.json( actualizarVisitas);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-module.exports = {crearBlog,actualizarBlog}
+const obtenerAllBlogs =asyncHandler(async(req,res)=>{
+  try {
+    const obtenerAllBlogs  = await Blog.find()
+    res.json(obtenerAllBlogs );
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+const eliminarBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const eliminarBlog = await Blog.findByIdAndDelete(id)
+    res.json(eliminarBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { crearBlog, actualizarBlog, obtenerBlog,obtenerAllBlogs,eliminarBlog };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,11 @@
 const express= require("express")
 const { authMiddleware, esAdmin } = require("../middleware/authMiddleware")
-const { crearBlog, actualizarBlog } = require("../controller/blogController")
+const { crearBlog, actualizarBlog, obtenerBlog, obtenerAllBlogs, eliminarBlog } = require("../controller/blogController")
 const router = express.Router()
 router.post("/",authMiddleware,esAdmin,crearBlog)
 router.put("/:id",authMiddleware,esAdmin,actualizarBlog)
+router.delete("/:id",authMiddleware,esAdmin,eliminarBlog)
+router.get("/:id",obtenerBlog)
+router.get("/",obtenerAllBlogs)
 
 module.exports  = router
